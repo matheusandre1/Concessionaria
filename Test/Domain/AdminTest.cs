9486550516f44b0f6457362b0a830a441c5783ef
Matheus Andre <matheusandr2@gmail.com>
2024-08-02 10:55:56 -0300

    adicionando test

---
+++ Test/Domain/AdminTest.cs
@@ -0,0 +1,27 @@
+﻿using agencia.Domain.Entities;
+
+namespace Test.Domain
+{
+    [TestClass]
+    public class AdminstradorTest
+    {
+        [TestMethod]
+        public void AdminTest()
+        {
+            var admin = new Admin();
+
+            admin.Id = 1;
+            admin.Email = "teste@teste.com";
+            admin.Senha = "123456";
+            admin.Perfil = "Adm";
+
+            Assert.AreEqual(1,admin.Id);
+            Assert.AreEqual("teste@teste.com", admin.Email);
+            Assert.AreEqual("123456", admin.Senha);
+            Assert.AreEqual("Adm", admin.Perfil);
+
+        }
+
+    }
+    
+}
